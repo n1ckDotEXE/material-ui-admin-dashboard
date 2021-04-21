3e077a2093bfe370d4fb5a5cedba21736e7f1cfd
n1ckDotEXE <admin@nicholasnam.com>
2021-04-21 10:51:10 -0400

    App.js setup with Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import { deepPurple } from "@material-ui/core/colors";
+import { Button } from "@material-ui/core";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: deepPurple["A200"],
+			contrastText: "#fff",
+		},
+		secondary: {
+			main: "#FFF5EE",
+			contrastText: "black",
+		},
+	},
+	typography: {
+		fontFamily: ["Roboto", "Helvetica Neue", "Arial", "sans-serif"].join(
+			","
+		),
+	},
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Button variant="contained" color="primary">
+				Primary
+			</Button>
+			<Button variant="contained" color="secondary">
+				Secondary
+			</Button>
+		</ThemeProvider>
+	);
 }
-
 export default App;
