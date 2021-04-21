56eb15f2de9effb3c2f983b9cf75f6a5c1da7351
n1ckDotEXE <admin@nicholasnam.com>
2021-04-21 12:19:53 -0400

    Lazy Loading complete

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
+import { NavLink } from "react-router-dom";
+
+import "./Navbar.css";
+
+const useStyles = makeStyles(() => ({
+	root: {
+		flexGrow: 1,
+	},
+	title: {
+		flexGrow: 1,
+	},
+}));
 
 function Navbar() {
-	return <div>Navbar</div>;
-}
+	const classes = useStyles();
 
+	return (
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography variant="h6" className={classes.title}>
+						Admin Dashboard
+					</Typography>
+
+					<NavLink
+						to="/login"
+						exact
+						className="nav-link"
+						activeClassName="active-nav-link"
+					>
+						<Button color="inherit">Login</Button>
+					</NavLink>
+
+					<NavLink
+						to="/sign-up"
+						exact
+						className="nav-link"
+						activeClassName="active-nav-link"
+					>
+						<Button color="inherit">Sign up</Button>
+					</NavLink>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
+}
 export default Navbar;
