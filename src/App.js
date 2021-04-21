1a53cec8a25290d51ebfae79e7fec9e00d978cc2
n1ckDotEXE <admin@nicholasnam.com>
2021-04-21 11:46:25 -0400

    Lazy loading added + Spinner fallback

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
 import { deepPurple } from "@material-ui/core/colors";
-import { Button } from "@material-ui/core";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Spinner from "./components/Spinner/Spinner";
+import MainRouter from "./MainRouter";
+import React from "react";
 
 const theme = createMuiTheme({
 	palette: {
@@ -24,13 +28,13 @@ const theme = createMuiTheme({
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<Button variant="contained" color="primary">
-				Primary
-			</Button>
-			<Button variant="contained" color="secondary">
-				Secondary
-			</Button>
+			<React.Suspense fallback={<Spinner />}>
+				<Router>
+					<MainRouter />
+				</Router>
+			</React.Suspense>
 		</ThemeProvider>
 	);
 }
+
 export default App;
